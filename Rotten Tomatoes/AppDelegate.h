53df66b03391dc55b498c548d4451ec6e0439a19
wseo88 <wseo88@gmail.com>
2015-02-02 20:13:40 -0800

    Initial Commit

---
+++ Rotten Tomatoes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Rotten Tomatoes
+//
+//  Created by William Seo on 2/2/15.
+//  Copyright (c) 2015 William Seo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Rotten Tomatoes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Rotten Tomatoes
+//
+//  Created by William Seo on 2/2/15.
+//  Copyright (c) 2015 William Seo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
